0e24ceb1b67913134cf44ead2502b52b57e753da
HamidAkrami <100194562+HamidAkrami@users.noreply.github.com>
2022-06-14 17:18:43 +0430

    dWallet

--- lib/app/presantation/pages/secret_phrase_page/secret_phrase_page.dart
+++ lib/app/presantation/pages/secret_phrase_page/secret_phrase_page.dart
@@ -49,36 +49,107 @@ class _SecretPhrasePageState extends State<SecretPhrasePage> {
               ],
             ),
             Expanded(
-                child: Text(
-              "Your Secret Phrase",
-              style: Themes.dark.textTheme.headline1,
-            )),
-            Expanded(
-                child: Container(
-              child: Text(
-                "Write down or copy these words in the right order and save them somewhere safe.",
-                style: Themes.dark.textTheme.subtitle1,
-                textAlign: TextAlign.center,
+              flex: 2,
+              child: Column(
+                children: [
+                  Text(
+                    "Your Secret Phrase",
+                    style: Themes.dark.textTheme.headline1,
+                  ),
+                  Container(
+                    child: Text(
+                      "Write down or copy these words in the right order and save them somewhere safe.",
+                      style: Themes.dark.textTheme.subtitle1,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Container(
+                    width: size.width * 0.75,
+                    child: Wrap(
+                      alignment: WrapAlignment.center,
+                      children: [
+                        ...mnemonic.asMap().entries.map((e) => Container(
+                              child: Row(
+                                children: [
+                                  Text((e.key + 1).toString()),
+                                  Text(
+                                    e.value,
+                                    style: Themes.dark.textTheme.subtitle1,
+                                  ),
+                                ],
+                              ),
+                            ))
+                      ],
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Clipboard.setData(ClipboardData(text: "Copy"));
+                      print("Copy");
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 100,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Copy",
+                            style: TextStyle(
+                                fontFamily: "OpenSans",
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xffFFD60A),
+                                fontSize: 24),
+                          ),
+                          Image.asset("assets/icons/copy.png"),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            )),
-            Expanded(
-                child: Container(
-              decoration: BoxDecoration(
-                  border: Border.all(width: 2, color: Colors.white)),
-              width: size.width * 0.75,
-              child: Wrap(),
-            )),
-            SizedBox(
-              height: 30,
             ),
             Expanded(
-              child: TextButton(
-                  onPressed: () {
-                    Clipboard.setData(ClipboardData(text: "Copy"));
-                  },
-                  child: Text(
-                    "Copy",
+              child: Column(
+                children: [
+                  Expanded(
+                      child: Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Color(0xffFF453A).withOpacity(0.1)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Do not expose your Secret Phrase",
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontFamily: "OpenSans",
+                              fontSize: 18,
+                              color: Color(0xffFF453A)),
+                        ),
+                        Text(
+                          "if someone has your Secret Phrase (12 words) they will have full access to your wallet.",
+                          style: TextStyle(
+                              fontFamily: "OpenSans",
+                              fontWeight: FontWeight.normal,
+                              color: Color(0xffFF453A)),
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ),
                   )),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: ElevatedButton(
+                              onPressed: () {}, child: Text("Continue"))),
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
